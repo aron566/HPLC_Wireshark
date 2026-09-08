034d67dcb034216c311149b32989fe80337f9722
aron566 <aron566@163.com>
2026-09-08 17:15:38 +0800

    v1.0.9:版本号三处同步 + update.json

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ static void decide_language() {
 int main(int argc, char* argv[]) {
     QCoreApplication::setOrganizationName("ZbMonitor");
     QCoreApplication::setApplicationName("BPLC_STA_Monitor");
-    QCoreApplication::setApplicationVersion("1.0.8");
+    QCoreApplication::setApplicationVersion("1.0.9");
 
     QApplication app(argc, argv);
     decide_language();
